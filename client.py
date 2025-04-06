c8f9ca70ce3bd03c12ec5e9cd44f6387eb67af03
Yaroslav Ozornin <Runboll023@gmail.com>
2025-04-06 14:08:00 +0500

    fixes

--- client.py
+++ client.py
@@ -3,13 +3,13 @@ import struct
 import time
 
 SERVER = 'localhost'
-PORT = 12345  # тот же, что в config.txt
+PORT = 123
 NTP_EPOCH = 2208988800
 
 def SNTP_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    request = b'\x1b' + 47 * b'\0'
-    client.sendto(request, (SERVER, PORT))
+    data = b'\x1b' + 47 * b'\0'  # Байтовый пакет, 48 байт ровно
+    client.sendto(data, (SERVER, PORT))
     data, _ = client.recvfrom(1024)
     timestamp = struct.unpack('!12I', data)[10] - NTP_EPOCH
     print("Время: ", time.ctime(timestamp))
